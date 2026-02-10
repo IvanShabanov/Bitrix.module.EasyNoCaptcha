9fd050dace0d0f9df1c0e02e8fe66fbe2bf821f0
Ivan Shabanov <Ivan.shabanov@gmail.com>
2026-02-10 12:22:46 +0700

    feat (Event.Setup): загрузка скрипта не блокирует отрисовку страницы

--- modules/is_pro.easy_no_captcha/lib/Events.php
+++ modules/is_pro.easy_no_captcha/lib/Events.php
@@ -27,7 +27,7 @@ class Events
 			return true;
 		}
 
-		\Bitrix\Main\Page\Asset::getInstance()->addString('<script data-id="ENC" data-skip-moving="true" src="'.$option['URL_PATH'].'/script.php"></script>');
+		\Bitrix\Main\Page\Asset::getInstance()->addString('<script defer data-id="ENC" data-skip-moving="true" src="'.$option['URL_PATH'].'/script.php"></script>');
 
 
 		$eventManager = \Bitrix\Main\EventManager::getInstance();
